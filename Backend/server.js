b6580a23057bd79500404a1594f2c0d0bf4d7063
Saimazhar12 <saimazhar1598@gmail.com>
2026-05-02 00:20:11 +0500

    added mern project folder

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+import app from './app.js';
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server runing on Port${process.env.PORT}`);
+});
+
+
---
+++ Frontend/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {data} from '../restApi.json'
+const Menu = () => {
+  return (
+    <>
+      <section className='menu' id='menu'>
+        <div className="container">
+            <div className="heading_section">
+                <h1 className="heading">POPULAR DISHES</h1>
+                <p>Discover our most loved dishes, crafted with high-quality ingredients—from flavorful grilled meats to fresh seafood and healthy breakfast, lunch, and dinner options. Perfect for any craving, anytime.</p>
+            </div>
+            <div className="dishes_container">
+                {
+                    data[0].dishes.map(element => (
+                        <div className="card" key={element.id}>
+                                <img src={element.image} alt={element.title} />
+                                <h3>{element.title}</h3>
+                                <button>{element.category}</button>
+                        </div>
+                    ))
+                }   
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Menu
